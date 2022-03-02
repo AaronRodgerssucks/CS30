ed482fc68f0112d3ec40ed29f115df2675e8e467
904243005 <904243005@L815WD170034.edu.cbe.ab.ca>
2022-03-02 11:25:27 -0700

    g

---
+++ Chapter 10/src/GUI.java
@@ -0,0 +1,27 @@
+
+public class employee 
+
+{
+
+	
+	//Jamal,456
+	private String (firstname)
+	private String (lastname)
+	
+	Public employee (fn, string wtf)
+	
+	firstname = Fn:
+	lastname = ln:
+		public string string
+		
+		abstract double pay (double period):
+			
+	public class manager
+	{
+		double yearlysalary; 
+		public manager(string,FN,LN, double sal)
+		sal: Nkn
+		//bro 
+		
+	}
+}
